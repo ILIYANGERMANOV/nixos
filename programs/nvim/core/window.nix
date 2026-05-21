857f81ea6b416bb16024f04e8049b631ed7891c5
Iliyan Germanov <iliyan@coinlist.co>
2026-05-21 14:52:11 +0300

    Add <leader> < and > to manage hor window size

--- programs/nvim/core/window.nix
+++ programs/nvim/core/window.nix
@@ -2,19 +2,75 @@ _:
 
 {
   keymaps = [
-    { mode = "n"; key = "<leader>h"; action = "<C-w>h"; options.desc = "Focus Left"; }
-    { mode = "n"; key = "<leader>l"; action = "<C-w>l"; options.desc = "Focus Right"; }
-    { mode = "n"; key = "<leader>j"; action = "<C-w>j"; options.desc = "Focus Down"; }
-    { mode = "n"; key = "<leader>k"; action = "<C-w>k"; options.desc = "Focus Up"; }
-    { mode = "n"; key = "<leader>H"; action = "<C-w>H"; options.desc = "Move Window Left"; }
-    { mode = "n"; key = "<leader>L"; action = "<C-w>L"; options.desc = "Move Window Right"; }
-    { mode = "n"; key = "<leader>J"; action = "<C-w>J"; options.desc = "Move Window Down"; }
-    { mode = "n"; key = "<leader>K"; action = "<C-w>K"; options.desc = "Move Window Up"; }
+    {
+      mode = "n";
+      key = "<leader>h";
+      action = "<C-w>h";
+      options.desc = "Focus Left";
+    }
+    {
+      mode = "n";
+      key = "<leader>l";
+      action = "<C-w>l";
+      options.desc = "Focus Right";
+    }
+    {
+      mode = "n";
+      key = "<leader>j";
+      action = "<C-w>j";
+      options.desc = "Focus Down";
+    }
+    {
+      mode = "n";
+      key = "<leader>k";
+      action = "<C-w>k";
+      options.desc = "Focus Up";
+    }
+    {
+      mode = "n";
+      key = "<leader>H";
+      action = "<C-w>H";
+      options.desc = "Move Window Left";
+    }
+    {
+      mode = "n";
+      key = "<leader>L";
+      action = "<C-w>L";
+      options.desc = "Move Window Right";
+    }
+    {
+      mode = "n";
+      key = "<leader>J";
+      action = "<C-w>J";
+      options.desc = "Move Window Down";
+    }
+    {
+      mode = "n";
+      key = "<leader>K";
+      action = "<C-w>K";
+      options.desc = "Move Window Up";
+    }
     {
       mode = "n";
       key = "<leader>wp";
       action = "<C-w>p";
       options.desc = "Jump to Previous Window";
+    }
+    {
+      mode = "n";
+      key = "<leader>>";
+      action = "<cmd>vertical resize +5<CR>";
+      options = {
+        desc = "Make window wider horizontally";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader><";
+      action = "<cmd>vertical resize -5<CR>";
+      options = {
+        desc = "Make window narrower horizontally";
+      };
     }
   ];
 
