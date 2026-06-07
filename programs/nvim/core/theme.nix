dadbd12e47880c9c4d917d5b2a749063ed925c91
iliyangermanov <iliyan.germanov971@gmail.com>
2026-06-07 22:31:48 +0300

    NeoVim: currently selected file on light contrast

--- programs/nvim/core/theme.nix
+++ programs/nvim/core/theme.nix
@@ -152,6 +152,8 @@ in
             vim.api.nvim_set_hl(0, "NotifyTRACETitle",  { fg = "#8839ef" })
             vim.api.nvim_set_hl(0, "NotifyTRACEIcon",   { fg = "#8839ef" })
             vim.api.nvim_set_hl(0, "NotifyTRACEBorder", { fg = "#8839ef" })
+            -- NvimTree selected file: surface1 bg (#bcc0cc) gives clear contrast on latte
+            vim.api.nvim_set_hl(0, "NvimTreeCursorLine", { bg = "#bcc0cc" })
           end,
         })
       '';
@@ -217,6 +219,7 @@ in
             vim.api.nvim_set_hl(0, "NotifyTRACETitle",  { fg = "#8839ef" })
             vim.api.nvim_set_hl(0, "NotifyTRACEIcon",   { fg = "#8839ef" })
             vim.api.nvim_set_hl(0, "NotifyTRACEBorder", { fg = "#8839ef" })
+            vim.api.nvim_set_hl(0, "NvimTreeCursorLine", { bg = "#bcc0cc" })
           end
           _set_notify_hl()
           vim.api.nvim_create_autocmd("ColorScheme", { pattern = "*", callback = _set_notify_hl })
