bd63c12cd513a6f8cef6edd45c3e00acec48d6da
iliyangermanov <iliyan.germanov971@gmail.com>
2026-06-07 23:27:22 +0300

    Add a comment about 'stay-centered'

--- programs/nvim/core/scrolling.nix
+++ programs/nvim/core/scrolling.nix
@@ -7,6 +7,7 @@
     neoscroll.enable = true;
   };
 
+  # Note: "stay-centered-nvim" might affect performance and breaks smooth scrolling
   extraPlugins = [ pkgs.vimPlugins.stay-centered-nvim ];
 
   extraConfigLua = ''
