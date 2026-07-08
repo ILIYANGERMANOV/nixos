b74392e2b7d929014fe42fc163c2a182c0919317
Iliyan Germanov <iliyan.germanov971@gmail.com>
2026-07-08 18:14:47 +0300

    chore: update and fix errors + warnings (#101)
    * Update flake.lock
    
    * Fix Homebrew error
    
    * Fix warnings

--- modules/home/nvim.nix
+++ modules/home/nvim.nix
@@ -1,4 +1,4 @@
-{ root, themeConfig, ... }:
+{ root, themeConfig, pkgs, ... }:
 let
   nvimStyle = {
     dark = "catppuccin_dark";
@@ -10,6 +10,9 @@ in
   programs.nixvim = {
     enable = true;
     defaultEditor = true;
+    # useGlobalPkgs already builds nixvim against the system pkgs; pin the
+    # source to match so nixvim doesn't warn about its follows-overridden nixpkgs.
+    nixpkgs.source = pkgs.path;
     myNixVim.style = nvimStyle;
     imports = [
       (import "${root}/programs/nvim")
